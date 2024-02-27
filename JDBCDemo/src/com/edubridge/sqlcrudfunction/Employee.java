9d17e980f12d3005c896b7d44a3d22bb825c6c65
IT-Admin <IT-Admin@lenovo-PC>
2024-02-27 12:41:58 +0530

    JDBC Commit

---
+++ JDBCDemo/src/com/edubridge/sqlcrudfunction/Employee.java
@@ -0,0 +1,39 @@
+package com.edubridge.sqlcrudfunction;
+
+import java.sql.*;
+
+public class Employee {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// STEP 1 - LOAD THE JDBC DRIVER
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		// STEP 2 - CONNECTION
+		
+		String url = "jdbc:mysql://localhost:3306/Management";
+		String user = "root";
+		String password = "root";
+		
+		//STEP 3 - CALL DRIVER MANAGER
+		 Connection conn = DriverManager.getConnection(url,user,password);
+		 
+		 System.out.println("Collecting Data from Employee Table");
+		 
+		 //The object is used for executing the static SQL statement
+		 
+		 Statement stmt = conn.createStatement();
+		 
+		 String sql = "select * from employee";
+		 
+		 ResultSet res = stmt.executeQuery(sql);
+		 while(res.next())
+		 {
+			 int empID = res.getInt("id");
+			 String empName = res.getString("name");
+			 int empAge = res.getInt("age");
+			 
+			 System.out.println(empID+"\t"+empName+"\t\t"+empAge);
+		 }
+	}
+
+}
